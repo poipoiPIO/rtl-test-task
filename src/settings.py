f36da6d7f29d1cb2f2e86fa909c8c79434252317
Lily Appely <lappee@yahoo.com>
2023-04-09 11:24:24 +0300

    [ADD] : Setting up constants from envitoment variables

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,11 @@
+import os
 
-DB_USERNAME = "lappely"
-DB_PASSWORD = "password"
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_PORT     = os.environ["DB_PORT"]
 
-DB_URL = "mongodb://{}:{}@127.0.0.1:27017".format(
-    DB_USERNAME, DB_PASSWORD
+DB_URL = "mongodb://{}:{}@127.0.0.1:{}".format(
+    DB_USERNAME, DB_PASSWORD, DB_PORT
 )
 
-TELEGRAM_BOT_TOKEN = ""
+TELEGRAM_BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
